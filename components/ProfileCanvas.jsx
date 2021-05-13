d475427ce3ebcf3e79d257d364fd742291254483
Junyoung Park <jypark@siliconbridge.co.kr>
2021-05-13 17:59:44 +0900

    feat: canvas circle

--- components/ProfileCanvas.jsx
+++ components/ProfileCanvas.jsx
@@ -1,35 +1,31 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react'
+import React, { useRef, useEffect, useMemo } from 'react'
 
 export default ({ url }) => {
   const ref = useRef()
-  const [image, setImage] = useState(null)
-  const ctx = useMemo(() => ref.current?.getContext('2d'), [ref])
-
-  useEffect(() => {
-    const img = new Image()
-    img.src = url
-    img.onload = () => setImage(img)
-  }, [url])
+  const ctx = useMemo(() => ref.current?.getContext('2d'), [ref.current])
 
   useEffect(() => {
     if (!ctx) return
-    if (!image) return
 
-    const x = image.width / 2
-    const y = image.height / 2
-    const radius = image.width / 4
+    const img = new Image()
+    img.src = url
+    img.onload = () => {
+      const x = img.width / 2
+      const y = img.height / 2
+      const radius = img.width * 0.9 / 2
 
-    ctx.drawImage(image, 0, 0)
-    ctx.beginPath()
-    ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
-    ctx.clip()
-    ctx.clearRect(
-      x - radius - 1,
-      y - radius - 1,
-      radius * 2 + 2,
-      radius * 2 + 2
-    )
-  }, [ctx, image])
+      ctx.drawImage(img, 0, 0)
+      ctx.beginPath()
+      ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+      ctx.clip()
+      ctx.clearRect(
+        x - radius - 1,
+        y - radius - 1,
+        radius * 2 + 2,
+        radius * 2 + 2
+      )
+    }
+  }, [url, ref.current])
 
-  return <canvas ref={ref} width={image?.width} height={image?.height} />
+  return <canvas ref={ref} width={400} height={400} />
 }
